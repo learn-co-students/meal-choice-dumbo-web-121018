ae9d6aa3829ce5e9b4f05045d7ca725246c1cc0f
synchronous-environment-3669 <kyu22@buffalo.edu>
2018-11-20 00:36:25 +0000

    Done.

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,2 +1,10 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
+
+
+def meal_choice(food ="meat")
+  if food == meat
+    return meat
+  else 
+    return vegan
+end
